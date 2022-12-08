19157f80be1325e166d0d5a1634af3e469152c6e
hawkgave <117901120+hawkgave@users.noreply.github.com>
2022-12-08 16:06:39 +0700

    Add files via upload

--- 2_D_71220932.py
+++ 2_D_71220932.py
@@ -5,12 +5,12 @@ p1= int(input("Masukkan model matematika yang diinginkan 1/2 : "))
 p2= int(input("Menampilkan table matematika dari angka: "))
 if (p1==1):
     a1=[1,2,3,4,5,6,7,8,9,10]
-    for i in range (1,11):
-        kali=i*p2
-        print(i, "x", p2, "=", kali)
+    for indeks in range (1,11):
+        kali=indeks*p2
+        print(indeks, "x", p2, "=", kali)
 
 elif (p1==2):
     a2=[50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65]
-    for i in range(50,66):
-        bagi=i/p2
-        print(i, ":", p2, "=", bagi)
+    for indeks in range(50,66):
+        bagi=indeks/p2
+        print(indeks, ":", p2, "=", bagi)
